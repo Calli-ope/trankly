afe72236999fb46ace5ebe0bac7e22d313215de1
Konrad <konradm.studium@gmail.com>
2024-01-26 21:32:39 +0100

    Add tabs

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,8 @@
 <script>
-    export let y;
+    /**
+	 * @type {number}
+	 */
+     export let y;
 
     let tabs = [
         {
@@ -8,26 +11,30 @@
         },
         {
             name: "About",
-            link: "/about"
+            link: "#about"
         },
         {
-            name: "Projects",
-            link: "/projects"
+            name: "Projekte",
+            link: "#projekte"
         },
         {
-            name: "Contact",
-            link: "/contact"
-        }
+            name: "Kontakt",
+            link: "#contact"
+        },
+        {
+            name: "Impressum",
+            link: "#Impressum"}
     ]
 </script>
 
-<header>
-    <h1>
+<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid ' + 
+(y > 0 ? ' py-4 bg-slate-950 border-violet-950' : ' py-6 bg-transparent border-transparent')}>
+    <h1 class="font-medium">
         Konrad Martens
     </h1>
-    <div class="">
+    <div class="sm:flex items-center gap-4 hidden">
         {#each tabs as tab, index}
-            <a>
+            <a href={tab.link} class="duration-200 hover:text-violet-400">
                 <p>{tab.name}</p>
             </a>
         {/each}
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,1 +1,12 @@
-konrad
+<script>
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+
+        </div>
+
+    </section>
+</main>
