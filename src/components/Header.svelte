c100a304d894682de9c8356e75195b9fb88fd5bb
Konrad <konradm.studium@gmail.com>
2024-01-26 21:07:19 +0100

    basic layout

---
+++ src/components/Header.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let y;
+
+    let tabs = [
+        {
+            name: "Home",
+            link: "/"
+        },
+        {
+            name: "About",
+            link: "/about"
+        },
+        {
+            name: "Projects",
+            link: "/projects"
+        },
+        {
+            name: "Contact",
+            link: "/contact"
+        }
+    ]
+</script>
+
+<header>
+    <h1>
+        Konrad Martens
+    </h1>
+    <div class="">
+        {#each tabs as tab, index}
+            <a>
+                <p>{tab.name}</p>
+            </a>
+        {/each}
+    </div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,30 @@
 <script>
     import "../app.css";
+	import Footer from "../components/Footer.svelte";
+	import Header from "../components/Header.svelte";
     export const ssr = false;
+
+    /**
+	 * @type {number}
+	 */
+    let y;
+    let innerHeight = 0;
+    let innerWidth = 0;
+
+    function goTop() {
+        document.body.scrollIntoView({ behavior: "smooth" });
+    }
   </script>
   
-<slot />
+  <div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+    <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " + (y > 0 ? 'opacity-full pointer-events-auto':'opacity-0 pointer-events-none')}>
+        <button on:click={goTop} class="ml-auto rounded-full bg-slate-900 text-violett-400 px3 sm:px-4 hover:bg-slate-800 cursor-pointer">
+            <i class="fa-solid fa-arrow-up grid place-items-center aspect-square" />
+        </button>
+    </div>
+    <Header y={y}/>
+    <slot />
+    <Footer />
+  </div>
+
+  <svelte:window bind:scrollY={y}  bind:innerHeight bind:innerWidth/>
