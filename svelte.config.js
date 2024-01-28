11649bdc2fe35b5fedb9ace2ecd043956b3137d6
Konrad <konradm.studium@gmail.com>
2024-01-28 22:58:17 +0100

    vecel setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
