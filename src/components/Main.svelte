1699898a314d7293ae3b2873bb31c2b808ab2078
Konrad <konradm.studium@gmail.com>
2024-01-28 21:09:03 +0100

    Page outline

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -5,43 +5,41 @@ import SideBar from './SideBar.svelte';
 
 <main class="flex flex-col flex-1 p-4">
   <SideBar />
-  <section id="introPage" class="flex flex-col items-center justify-center py-8 sm:py-14">
-    <div class="flex flex-col items-center gap-10 md:gap-8 lg:gap-10 leading-loose">
+  <section id="/" class="flex flex-col items-center justify-center py-8 sm:py-14">
+    <div class="flex flex-col items-center gap-20 leading-loose">
       <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl text-center">
         Ranking der besten <span class="hover:text-euBlue">europäischen</span> <br/> Hochgeschwindigkeitszüge
       </h2>
-      <div class="text-2xl sm:text-lg md:text-xl text-center leading-relaxed">
-        In diesem Ranking werde ich die besten Hochgeschwindigkeitszüge nach den Kriterien <br/>
-        <p class="text-left">
-        <span class="hover:text-teal-400">Komfort,<br/></span>
-        <span class="hover:text-teal-400">Geschwindigkeit,<br/></span>
-        <span class="hover:text-teal-400">Ausstattung,<br/></span>
-        <span class="hover:text-teal-400">& Service<br/></span>
-        </p>
-        bewerten.
+      <div class="gap-1 flex flex-col items-center">
+      <p class="text-2xl sm:text-lg md:text-xl text-center">
+        In diesem Ranking werde ich die besten Hochgeschwindigkeitszüge <i class="fa-solid fa-train fa-beat-fade fa-lg" style="color: #74C0FC;"></i> nach den folgenden Kriterien bewerten:
+      </p>
+      <ul class="text-left list-disc text-xl">
+        <li class="hover:text-teal-400">Komfort<br/></li>
+        <li class="hover:text-teal-400">Geschwindigkeit<br/></li>
+        <li class="hover:text-teal-400">Ausstattung<br/></li>
+        <li class="hover:text-teal-400">& Service<br/></li>
+      </ul>
       </div>
     </div>
   </section>
 
-  
-
-
-    <!--<section>
-        <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-            <div class="md:flex">
-              <div class="md:shrink-0">
-                <img class="h-48 w-full object-cover md:h-full md:w-48" src="/img/building.jpg" alt="Modern building architecture">
-              </div>
-              <div class="p-8">
-                <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
-                <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accommodation for your team</a>
-                <p class="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
-              </div>
-            </div>
-        </div>
-        <FlexiblePanel title="My Flexible Panel">
-             Additional content can be added here if needed
-        </FlexiblePanel>
-
-    </section> -->
+  <section class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div class="md:flex">
+      <div class="p-8">
+        <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold"><i class="fa-solid fa-exclamation fa-bounce mr-1" style="color: #f84407;"></i>Disclaimer</div>
+          <p class="mt-2 text-slate-500">Das Ranking basiert ausschließlich auf eigenen Erfahrungen und erhebt keinen Anspruch auf Vollständigkeit.</p>
+      </div>
+    </div>
+  </section>
+  <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
+  <section id="öbb" class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl py-8 sm:py-14">
+    <div class="md:flex">
+      <div class="p-8">
+        <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold"><i class="fa-solid fa-exclamation fa-bounce mr-1" style="color: #f84407;"></i>Disclaimer</div>
+          <p class="mt-2 text-slate-500">Das Ranking basiert ausschließlich auf eigenen Erfahrungen und erhebt keinen Anspruch auf Vollständigkeit.</p>
+      </div>
+    </div>
+  </section>
+  <hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">
 </main>
--- src/components/SideBar.svelte
+++ src/components/SideBar.svelte
@@ -2,29 +2,29 @@
 
 </script>
 
-<aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen" aria-label="Sidebar">
+<aside class="fixed top-0 left-0 z-40 w-64 h-screen" aria-label="Sidebar">
     <div class="h-full px-3 py-4 overflow-y-auto bg-slate-950">
-       <a href="/" class="flex items-center ps-5 mb-8">
+        <a href="/" class="flex items-center ps-5 mb-8">
             <svg xmlns="http://www.w3.org/2000/svg" class="lg:w-10 lg:h-10 sm:h-6 sm:w-6 fill-current text-yellow-400 mr-4" viewBox="0 0 20 20">
                 <path d="M15 9a3 3 0 0 0 3-3h2a5 5 0 0 1-5.1 5 5 5 0 0 1-3.9 3.9V17l5 2v1H4v-1l5-2v-2.1A5 5 0 0 1 5.1 11H5a5 5 0 0 1-5-5h2a3 3 0 0 0 3 3V4H2v2H0V2h5V0h10v2h5v4h-2V4h-3v5z"/>
             </svg>
             <span class="self-center text-2xl font-semibold text-white">Ranking</span>
-       </a>
+        </a>
        
         <ul class="space-y-2 font-medium">
             <li>
-                <a href="#öbb" class="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 hover:text-violet-300">
-                    <span class="ms-3">1. ÖBB</span>
+                <a href="öbb" class="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 hover:text-violet-300">
+                    <span class="ms-3 text-xl">1. ÖBB</span>
                 </a>
             </li>
             <li>
                 <a href="#eurostar" class="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 hover:text-violet-300">
-                    <span class="flex-1 ms-3 whitespace-nowrap">2. Eurostar</span>
+                    <span class="ms-3 text-xl">2. Eurostar</span>
                 </a>
             </li>
             <li>
-                <a href="#ibb" class="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 hover:text-violet-300">
-                    <span class="flex-1 ms-3 whitespace-nowrap">Italienische Bahn</span>
+                <a href="ibb" class="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 hover:text-violet-300">
+                    <span class="ms-3 text-xl">Italienische Bahn</span>
                 </a>
             </li>
         </ul>
