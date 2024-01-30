aab70478a7c78920024371246b1b530ed8f7e9a0
Konrad <konradm.studium@gmail.com>
2024-01-30 19:15:02 +0100

    add Impressum

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,13 +1,15 @@
 <script>
-    export let impressumLink = "#";
-    export let datenschutzLink = "#";
+    export let home = "/";
+    export let impressum = "/Impressum";
+    export let datenschutz = "#";
 </script>
 
 <footer class="bg-slate-950 rounded-lg shadow m-4 ">
     <div class="w-full mx-auto max-w-screen-xl p-4">
         <ul class="flex flex-wrap justify-center ml-40 mt-3 text-sm font-medium text-white">
-                <li><a class="px-4 hover:text-violet-300 hover:underline" href={impressumLink}>Impressum</a></li>
-                <li><a class="px-4 hover:text-violet-300 hover:underline" href={datenschutzLink}>Datenschutz</a></li>
+                <li><a class="px-4 hover:text-violet-300 hover:underline" href={home}>Home</a></li>
+                <li><a class="px-4 hover:text-violet-300 hover:underline" href={impressum}>Impressum</a></li>
+                <li><a class="px-4 hover:text-violet-300 hover:underline" href={datenschutz}>Datenschutz</a></li>
         </ul>
     </div>
 </footer>
---
+++ src/routes/Impressum/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+    export let title = "Impressum";
+</script>
+
+<main>
+    <h1>{title}</h1>
+
+    <section>
+        <h2>Angaben gemäß § 5 TMG:</h2>
+        <p>Ihr Name</p>
+        <p>Ihre Straße 123</p>
+        <p>12345 Ihre Stadt</p>
+        <p>Telefon: 01234 567890</p>
+        <p>E-Mail: info@example.com</p>
+    </section>
+
+    <section>
+        <h2>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</h2>
+        <p>Ihr Name</p>
+        <p>Ihre Straße 123</p>
+        <p>12345 Ihre Stadt</p>
+    </section>
+
+    <section>
+        <h2>Haftungsausschluss (Disclaimer)</h2>
+        <h3>Haftung für Inhalte</h3>
+        <p>...</p>
+        <h3>Haftung für Links</h3>
+        <p>...</p>
+        <h3>Urheberrecht</h3>
+        <p>...</p>
+    </section>
+
+    <section>
+        <h2>Datenschutzerklärung</h2>
+        <p>...</p>
+    </section>
+</main>
+
+<style>
+    /* Add your custom styles here */
+</style>
