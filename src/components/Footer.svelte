0046e51ee155ccb1190542efa0e86c833deea46e
Konrad <konradm.studium@gmail.com>
2024-01-30 16:01:14 +0100

    add footer and fix button-top

---
+++ src/components/Footer.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let impressumLink = "#";
+    export let datenschutzLink = "#";
+</script>
+
+<footer class="bg-slate-950 rounded-lg shadow m-4 ">
+    <div class="w-full mx-auto max-w-screen-xl p-4">
+        <ul class="flex flex-wrap justify-center ml-40 mt-3 text-sm font-medium text-white">
+                <li><a class="px-4 hover:text-violet-300 hover:underline" href={impressumLink}>Impressum</a></li>
+                <li><a class="px-4 hover:text-violet-300 hover:underline" href={datenschutzLink}>Datenschutz</a></li>
+        </ul>
+    </div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
   </script>
   
   <div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
-    <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " + (y > 0 ? 'opacity-full pointer-events-auto':'opacity-0 pointer-events-none')}>
+    <div class={"fixed bottom-20 right-0 duration-200 flex p-10 z-[1] " + (y > 0 ? 'opacity-full pointer-events-auto':'opacity-0 pointer-events-none')}>
         <button on:click={goTop} class="ml-auto rounded-full bg-slate-900 text-violett-400 px3 sm:px-4 hover:bg-slate-800 cursor-pointer">
             <i class="fa-solid fa-arrow-up grid place-items-center aspect-square" />
         </button>
