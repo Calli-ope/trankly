d8efd519a316a40c42be43f85ccabb1745727941
Konrad <konradm.studium@gmail.com>
2024-02-23 16:26:46 +0100

    Add landing-page and Remove unused classes

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     export let home = "/";
-    export let impressum = "/Impressum";
-    export let datenschutz = "/Datenschutz";
+    export let impressum = "/impressum";
+    export let datenschutz = "/datenschutz";
 </script>
 
-<footer class="bg-slate-950 rounded-lg mb-4 mt-auto">
+<footer class="bg-slate-950 rounded-lg mb-2 mt-auto">
     <div class="mx-auto p-4">
         <ul class="flex flex-wrap justify-center my-2 text-sm font-medium text-white">
             <li><a class="px-4 hover:text-violet-300 hover:underline" href={home}>Home</a></li>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,7 +4,5 @@
 
 <header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-center border border-solid rounded-lg ' + 
 (y > 0 ? ' py-4 bg-slate-950 border-violet-950' : ' py-6 bg-transparent border-transparent')}>
-    <a href="/">
-        <h1 class="font-extrabold text-white text-3xl">Konrads Rankings</h1>
-    </a>
+    <h1 class="font-extrabold text-white text-4xl">Konrads Rankings</h1>
 </header>
