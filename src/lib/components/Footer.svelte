5ad3b0e02c442ac2148e1dc380c36c1ae493f768
Konrad <konradm.studium@gmail.com>
2024-02-26 21:17:42 +0100

    Add map to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     export let home = "/";
     export let ranking = "/ranking";
+    export let map = "/map";
     export let impressum = "/Impressum";
     export let datenschutz = "/Datenschutz";
 </script>
@@ -10,6 +11,7 @@
         <ul class="flex flex-wrap justify-center my-2 text-sm font-medium text-white">
             <li><a class="px-4 dark:hover:text-violet-300 hover:text-violet-700 hover:underline" href={home}>Home</a></li>
             <li><a class="px-4 dark:hover:text-violet-300 hover:text-violet-700 hover:underline" href={ranking}>Ranking</a></li>
+            <li><a class="px-4 dark:hover:text-violet-300 hover:text-violet-700 hover:underline" href={map}>Map</a></li>
             <li><a class="px-4 dark:hover:text-violet-300 hover:text-violet-700 hover:underline" href={impressum}>Impressum</a></li>
             <li><a class="px-4 dark:hover:text-violet-300 hover:text-violet-700 hover:underline" href={datenschutz}>Datenschutz</a></li>
         </ul>
